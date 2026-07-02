21ccb24dd141b939e2ea7959ed8b5d6bc984e46b
Jonas <livejo0045@gmail.com>
2026-07-02 12:21:30 +0100

    Create content view.swift

---
+++ content view.swift
@@ -0,0 +1,19 @@
+//
+//  content view.swift
+//  
+//
+//  Created by Jonas on 02/07/2026.
+//
+
+import swuftUI
+import charts
+
+struct contentview: View {
+    @stateobject private var viewModel = statsViewModel()
+    @state privete var showingSettings = false
+    
+    var body: some View {
+        NavigationStack {
+            Group {
+        }
+    }
