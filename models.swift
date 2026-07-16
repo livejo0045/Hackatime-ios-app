5ab3839adec03c54d79c090e90394bf43df7be93
Jonas <livejo0045@gmail.com>
2026-07-16 18:39:04 +0100

    Create models.swift

---
+++ models.swift
@@ -0,0 +1,21 @@
+//
+//  models.swift
+//  
+//
+//  Created by Jonas on 16/07/2026.
+//
+
+import Foundation
+
+struct HackatimeStats: Codable {
+    let totalSeconds: Double
+    let totalMinutes: Double
+    let totalHours: Double
+    let dailyAverage: Double
+    let languages: [StatItem]
+    let projects: [StatItem]
+    let editors: [StatItem]
+    
+    enum CodingKeys: String, CodingKey {
+        case totalSconds = "total_ "
+    }
