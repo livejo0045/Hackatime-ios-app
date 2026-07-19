40ec469bf6ed72431601a835838de1c0e89ac176
Jonas <livejo0045@gmail.com>
2026-07-19 22:03:36 +0100

    Update models.swift

--- models.swift
+++ models.swift
@@ -20,4 +20,16 @@ struct HackatimeStats: Codable {
         case totalSconds = "total_seconds"
         case totalMinutes= "total_minutes"
         case totalHours = "total_hours"
+        case dailyAverage = "daily_average" 
+    }
+
+    var formattedTotal: String
+    init(totalSeconds: Double, totalMinutes: Double,totalHours: Double, dailyAverage: Double, languages: [StatItem], projects: [StatItem], editors: [StatItem]) {
+        self.totalSeconds = totalSeconds
+        self.totalMinutes = totalMinutes
+        self.totalHours = totalHours
+        self.dailyAverage = dailyAverage
+        self.languages = languages
+        self.projects = projects
+        self.editors = editors
     }
